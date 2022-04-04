6d91eeb4d25947ed4d1ed47d9305722d0b21c71d
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-04-04 14:56:37 +0300

    feat: добавить императивные обработчики попапам

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,32 @@
 function Main() {
+
+    function handleEditAvatarClick() {
+        const popup = document.querySelector('.popup_avatar')
+        popup.classList.add('popup_opened')
+    }
+
+    function handleEditProfileClick() {
+        const popup = document.querySelector('.popup_edit')
+        popup.classList.add('popup_opened')
+    }
+
+    function handleAddPlaceClick() {
+        const popup = document.querySelector('.popup_add')
+        popup.classList.add('popup_opened')
+    }
+
     return (
         <main className="main">
             <section className="profile">
-                <button className="profile__avatar-box">
+                <button className="profile__avatar-box" onClick={handleEditAvatarClick}>
                     <img className="profile__avatar" src="images/no-avatar.png" alt="Аватар" />
                 </button>
                 <div className="profile__info">
                     <h1 className="profile__name">Загрузка ...</h1>
-                    <button className="profile__edit-button" type="button" aria-label="Открыть"></button>
+                    <button className="profile__edit-button" type="button" aria-label="Открыть" onClick={handleEditProfileClick}></button>
                     <p className="profile__about">Загрузка ...</p>
                 </div>
-                <button className="profile__add-button" type="button" aria-label="Открыть"></button>
+                <button className="profile__add-button" type="button" aria-label="Открыть" onClick={handleAddPlaceClick}></button>
 
                 <div className="profile__popup popup popup_edit">
                     <div className="popup__container">
@@ -19,12 +35,14 @@ function Main() {
                         <form className="popup__form" action="#" name="edit" novalidate>
                             <label className="popup__form-label">
                                 <input className="popup__input popup__input_value_name" id="name-input" type="text" name="name"
-                                    required minlength="2" maxlength="40" autocomplete="off" />
+                                    required minlength="2" maxlength="40" autocomplete="off"
+                                    placeholder="Ваше имя" />
                                 <span className="popup__input-error name-input-error"></span>
                             </label>
                             <label className="popup__form-label">
                                 <input className="popup__input popup__input_value_about" id="about-input" type="text"
-                                    name="about" required minlength="2" maxlength="200" autocomplete="off" />
+                                    name="about" required minlength="2" maxlength="200" autocomplete="off"
+                                    placeholder="Пару слов о себе" />
                                 <span className="popup__input-error about-input-error"></span>
                             </label>
 
